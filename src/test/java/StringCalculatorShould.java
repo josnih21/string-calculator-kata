d3ae8eb581cd18fd20b261d6496023140bc67e7a
José Ángel <josnih@gmail.com>
2020-05-11 15:04:24 +0100

    testing

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,12 +1,15 @@
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorShould {
-
-    @Test
-    public void it_works(){
-        assertThat("**").isEqualTo("**");
-    }
+/*/
+    TO-DO LIST
+    - Empty string --> "";
+    - "1" --> 1;
+    - "3,2" --> 5;
+ */
 
 }
