03d8d1e820406f5870297cdc431b54b38280a0cd
José Ángel <josnih@gmail.com>
2020-05-11 14:54:17 +0100

    base project

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -7,32 +7,5 @@ import static java.lang.Integer.parseInt;
 public class StringCalculator {
 
 
-    public static double add(String text) {
-        if (text.isEmpty()) {
-            return 0;
-        }
-        boolean doesntContainsComma = !text.contains(",");
-        if (doesntContainsComma) {
-            return Double.parseDouble(text);
-        }
-
-        String[] numbers = text.split(",");
-        List<String> numbersList = Arrays.asList(numbers);
-        List<Double> numbersListParsed = numbersList.stream()
-                .map(s -> Double.parseDouble(s))
-                .collect(Collectors.toList());
-        int sum = 0;
-        sum += addNumbers(numbersListParsed);
-        return sum;
-    }
-
-    public static double addNumbers(List<Double> numbers) {
-        if (numbers.size() == 1) {
-            return numbers.get(0);
-        }
-        double sum = 0;
-        sum += numbers.get(0);
-        numbers.remove(0);
-        return sum += addNumbers(numbers);
-    }
+    public static double add(String text) {return 0}
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -12,21 +12,4 @@ public class StringCalculatorShould {
     - "3,2" --> 5;
  */
 
-    @Test
-    public void return_zero_when_string_is_empty(){
-        assertThat(StringCalculator.add("")).isEqualTo(0);
-    }
-
-    @Test
-    public void return_same_value_when_one_value(){
-        assertThat(StringCalculator.add("1")).isEqualTo(1);
-        assertThat(StringCalculator.add("1.1")).isEqualTo(1.1);
-    }
-
-    @Test
-    public void return_added_input_values(){
-        assertThat(StringCalculator.add("1,2")).isEqualTo(3);
-        assertThat(StringCalculator.add("1,2,3")).isEqualTo(6);
-    }
-
 }
