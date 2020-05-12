aab74e813ec0e1e8c60d0675cfd4bae3a0bdb577
José Ángel <josnih@gmail.com>
2020-05-12 13:16:13 +0100

    added negative control without refactoring

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,47 +1,55 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class StringCalculator {
-    static String[] numbers;
     static String delimiter = ",";
-
-    public static double add(String text) {
+    public static String add(String text) {
+        String[] numbers;
+        String negativeList = "";
         if (text.isEmpty()) {
-            return 0;
+            return "0";
         }
         boolean doesntContainsComma = !text.contains(",");
         if (doesntContainsComma && !text.contains("\n")) {
-            return Double.parseDouble(text);
+            return Float.parseFloat(text)+"";
         }
 
+        if(text.endsWith(delimiter)){
+            return "Number expected but EOF found";
+        }
         if (text.contains("//")) {
             delimiter = text.substring(2,text.indexOf("\n"));
-            System.out.println(delimiter);
             String replacedText = text.replaceAll("//+"+delimiter+"\n", "");
             numbers = replacedText.split("["+delimiter+"\\n]");
-            System.out.println(Arrays.toString(numbers));
         } else {
             numbers = text.split(delimiter+"|\\n");
-            System.out.println(Arrays.toString(numbers));
         }
 
         List<String> numbersList = Arrays.asList(numbers);
-        List<Double> numbersListParsed = numbersList.stream()
-                .map(Double::parseDouble)
+        List<Float> numbersListParsed = numbersList.stream()
+                .map(Float::parseFloat)
                 .collect(Collectors.toList());
-        int sum = 0;
+        SortedSet<Float> sortedList = new TreeSet<>(numbersListParsed);
+        if(sortedList.first() < 0){
+            for (float number:numbersListParsed) {
+                if(number < 0){
+                    negativeList +=number+",";
+                }
+            }
+            return "Negative numbers not allowed : "+negativeList;
+        }
+        float sum = 0;
         sum += addNumbers(numbersListParsed);
-        return sum;
+        return sum+"";
     }
 
 
-    public static double addNumbers(List<Double> numbers) {
+    public static double addNumbers(List<Float> numbers) {
         if (numbers.size() == 1) {
             return numbers.get(0);
         }
-        double sum = 0;
+        float sum = 0;
         sum += numbers.get(0);
         numbers.remove(0);
         return sum += addNumbers(numbers);
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -15,32 +15,43 @@ public class StringCalculatorShould {
 
     @Test
     public void return_zero_when_string_is_empty(){
-        assertThat(StringCalculator.add("")).isEqualTo(0);
+        assertThat(StringCalculator.add("")).isEqualTo("0");
     }
 
     @Test
     public void return_same_value_when_one_value(){
-        assertThat(StringCalculator.add("1")).isEqualTo(1);
-        assertThat(StringCalculator.add("1.1")).isEqualTo(1.1);
+        assertThat(StringCalculator.add("1")).isEqualTo("1.0");
+        assertThat(StringCalculator.add("1.1")).isEqualTo("1.1");
     }
 
     @Test
     public void return_added_input_values(){
-        assertThat(StringCalculator.add("1,2,3")).isEqualTo(6);
-        assertThat(StringCalculator.add("3,3,3,3,3")).isEqualTo(15);
+        assertThat(StringCalculator.add("1,2,3")).isEqualTo("6.0");
+        assertThat(StringCalculator.add("3,3,3,3,3")).isEqualTo("15.0");
     }
 
     @Test
     public void return_add_with_different_delimiters(){
-        assertThat(StringCalculator.add("1,2\n3")).isEqualTo(6);
-        assertThat(StringCalculator.add("1\n2\n3")).isEqualTo(6);
+        assertThat(StringCalculator.add("1,2\n3")).isEqualTo("6.0");
+        assertThat(StringCalculator.add("1\n2\n3")).isEqualTo("6.0");
     }
 
     @Test
+    public void return_error_message_when_end_with_delimiter(){
+        assertThat(StringCalculator.add("1,2,")).isEqualTo("Number expected but EOF found");
+    }
+    @Test
     public void return_add_with_custom_delimiters(){
-        assertThat(StringCalculator.add("//.\n1.2.3")).isEqualTo(6);
-        assertThat(StringCalculator.add("//;\n1;2;3")).isEqualTo(6);
-        assertThat(StringCalculator.add("//k\n1k2k3k")).isEqualTo(6);
+        assertThat(StringCalculator.add("//.\n1.2.3")).isEqualTo("6.0");
+        assertThat(StringCalculator.add("//;\n1;2;3")).isEqualTo("6.0");
+        assertThat(StringCalculator.add("//;\n1;2\n3")).isEqualTo("6.0");
+        assertThat(StringCalculator.add("//k\n1k2k3k")).isEqualTo("6.0");
+        assertThat(StringCalculator.add("//.\n4.2.3")).isEqualTo("9.0");
+    }
+    @Test
+    public void return_error_message_when_negative_numbers_provided(){
+        assertThat(StringCalculator.add("-1,2,3")).isEqualTo("Negative numbers not allowed : -1.0,");
+        assertThat(StringCalculator.add("-2,3,4,-5")).isEqualTo("Negative numbers not allowed : -2.0,-5.0,");
     }
 
 }
