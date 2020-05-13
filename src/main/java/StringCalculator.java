824fd0e9ee1dc8bf3396be43b5ba2a58586285de
José Ángel <josnih@gmail.com>
2020-05-13 16:09:33 +0100

    before method added

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 public class StringCalculator {
     static String delimiter = ",";
 
-    public static String add(String text) {
+    public String add(String text) {
         String[] numbers;
         if (text.isEmpty()) { return "0"; }
         boolean doesntContainsComma = !text.contains(",");
@@ -24,7 +24,7 @@ public class StringCalculator {
         return sum + "";
     }
 
-    private static double operateWithNumbers(List<Float> numbers,Operation operation) {
+    private double operateWithNumbers(List<Float> numbers,Operation operation) {
         if (numbers.size() == 1) { return numbers.get(0); }
         float sum = 0;
         switch (operation){
@@ -42,13 +42,12 @@ public class StringCalculator {
         return sum;
     }
 
-
-    private static String negativeNumbers(List<Float> numbers) {
+    private String negativeNumbers(List<Float> numbers) {
         List<Float> negativeList = numbers.stream().filter(x -> x < 0).collect(Collectors.toList());
         return negativeList.stream().map(String::valueOf).collect(Collectors.joining(","));
     }
 
-    private static String[] splittedArrayOfValues(String text){
+    private String[] splittedArrayOfValues(String text){
         if (text.contains("//")) {
             delimiter = text.substring(2, text.indexOf("\n"));
             String replacedText = text.replaceAll("//+" + delimiter + "\n", "");
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,3 +1,5 @@
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,50 +12,58 @@ public class StringCalculatorShould {
     - "3,2" --> 5;
     - check separator with more than one character of lenght
  */
+    StringCalculator stringCalculator = null;
+
+    @BeforeEach
+    public void setup(){
+        stringCalculator = new StringCalculator();
+    }
 
     @Test
     public void return_zero_when_string_is_empty(){
-        assertThat(StringCalculator.add("")).isEqualTo("0");
+        assertThat(stringCalculator.add("")).isEqualTo("0");
     }
 
     @Test
     public void return_same_value_when_one_value(){
-        assertThat(StringCalculator.add("1")).isEqualTo("1.0");
-        assertThat(StringCalculator.add("1.1")).isEqualTo("1.1");
+        assertThat(stringCalculator.add("1")).isEqualTo("1.0");
+        assertThat(stringCalculator.add("1.1")).isEqualTo("1.1");
     }
 
     @Test
     public void return_added_input_values(){
-        assertThat(StringCalculator.add("1,2,3")).isEqualTo("6.0");
-        assertThat(StringCalculator.add("3,3,3,3,3")).isEqualTo("15.0");
+        assertThat(stringCalculator.add("1,2,3")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("3,3,3,3,3")).isEqualTo("15.0");
     }
 
     @Test
     public void return_add_with_different_delimiters(){
-        assertThat(StringCalculator.add("1,2\n3")).isEqualTo("6.0");
-        assertThat(StringCalculator.add("1\n2\n3")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("1,2\n3")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("1\n2\n3")).isEqualTo("6.0");
     }
 
     @Test
     public void return_error_message_when_end_with_delimiter(){
-        assertThat(StringCalculator.add("1,2,")).isEqualTo("Number expected but EOF found");
+        assertThat(stringCalculator.add("1,2,")).isEqualTo("Number expected but EOF found");
     }
+
     @Test
     public void return_add_with_custom_delimiters(){
-        assertThat(StringCalculator.add("//.\n1.2.3")).isEqualTo("6.0");
-        assertThat(StringCalculator.add("//;\n1;2;3")).isEqualTo("6.0");
-        assertThat(StringCalculator.add("//;\n1;2\n3")).isEqualTo("6.0");
-        assertThat(StringCalculator.add("//k\n1k2k3k")).isEqualTo("6.0");
-        assertThat(StringCalculator.add("//.\n4.2.3")).isEqualTo("9.0");
+        assertThat(stringCalculator.add("//.\n1.2.3")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("//;\n1;2;3")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("//;\n1;2\n3")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("//k\n1k2k3k")).isEqualTo("6.0");
+        assertThat(stringCalculator.add("//.\n4.2.3")).isEqualTo("9.0");
     }
+
     @Test
     public void return_error_message_when_negative_numbers_provided(){
-        assertThat(StringCalculator.add("-1,2,3")).isEqualTo("Negative numbers not allowed : -1.0");
-        assertThat(StringCalculator.add("-2,3,4,-5")).isEqualTo("Negative numbers not allowed : -2.0,-5.0");
+        assertThat(stringCalculator.add("-1,2,3")).isEqualTo("Negative numbers not allowed : -1.0");
+        assertThat(stringCalculator.add("-2,3,4,-5")).isEqualTo("Negative numbers not allowed : -2.0,-5.0");
     }
 
     @Test void return_result_of_multiplicate_provided_values(){
-        assertThat(StringCalculator.add("1,2")).isEqualTo("2.0");
+        assertThat(stringCalculator.add("1,2")).isEqualTo("2.0");
     }
 
 }
